abc52545af46fd358d84aa50a9b967403ea7ed09
Andrew Patton <anpatt7@gmail.com>
2025-11-29 13:40:47 -0800

    eod 11.29, making progress

--- src/blyjections/yoy.py
+++ src/blyjections/yoy.py
@@ -1,7 +1,9 @@
 import polars as pl
 
 
-def get_consecutive_seasons(frame: pl.DataFrame, player_col: str = "player_id", season_col: str = "Season") -> pl.DataFrame:
+def get_consecutive_seasons(
+    frame: pl.DataFrame, player_col: str = "player_id", season_col: str = "Season"
+) -> pl.DataFrame:
     """Returns all consecutive player-seasons, includinding those with gaps between. Ex: 2021-2022, and 2024-2025
 
     Args:
@@ -10,19 +12,22 @@ def get_consecutive_seasons(frame: pl.DataFrame, player_col: str = "player_id", 
         season_col (str, optional): Defaults to "Season, the FanGraphs format"
 
     Returns:
-        pl.DataFrame: _description_
+        pl.DataFrame:
     """
-    
+
     result = (
         frame.sort(player_col, season_col)
         .with_columns(
             (
-                pl.col(season_col) - pl.col("season").rank("ordinal").over(player_col)
-            ).alias("stretch_id")
+                pl.col(season_col) - pl.col(season_col).rank("ordinal").over(player_col)
+            ).alias("stretch_num")
         )
-        .with_columns(pl.len().over(player_col, "stretch_id").alias("stretch_len"))
+        .with_columns(
+            pl.col("stretch_num").rank("dense").over(player_col).alias("stretch_id"),
+            pl.len().over(player_col, "stretch_num").alias("stretch_len"),
+        )
         .filter(pl.col("stretch_len") > 1)
-        .drop("stretch_id", "stretch_len")
+        .drop("stretch_len", "stretch_num")
     )
 
     return result
